95f5b8d07fe31eabc9fc4a999cd2b4705d0d21a6
S. Amaador <soufiane.amaador@hva.nl>
2020-09-21 10:55:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel2task1/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.madlevel2task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.madlevel2task1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews(){
+        binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(2,1)
+        binding.rvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i],
+            Place.PLACE_RES_DRAWABLE_IDS[i]))
+        }
+
+        placeAdapter.notifyDataSetChanged()
+    }
+}
